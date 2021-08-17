2e2ad11d0f31ba70f66600bc77ec2733938b05bd
sksamuel <sam@sksamuel.com>
2021-08-17 09:41:24 -0500

    Bumped versions

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -4,14 +4,14 @@ object Libs {
    const val org = "io.kotest.extensions"
 
    object Kotest {
-      private const val version = "4.4.3"
+      private const val version = "4.6.1"
       const val Junit5 = "io.kotest:kotest-runner-junit5-jvm:$version"
       const val Assertions = "io.kotest:kotest-assertions-core:$version"
       const val Api = "io.kotest:kotest-framework-api:$version"
    }
 
    object MockServer {
-      private const val version = "5.11.2"
+      private const val version = "5.11.3"
       const val netty = "org.mock-server:mockserver-netty:$version"
       const val javaClient = "org.mock-server:mockserver-client-java:$version"
    }
